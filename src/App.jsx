9a1bde1477e018d256c0e40c41be6ab16e9a57f7
Harsha Nimeda Sirithunga <harshanimeda@gmail.com>
2026-07-22 00:33:27 +0530

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,46 @@
 import { Routes, Route } from "react-router-dom";
 
 import Login from "./pages/Login";
-
 import Register from "./pages/Register";
-
 import WaitingApproval from "./pages/WaitingApproval";
-
+import Dashboard from "./pages/Dashboard";
 import ProtectedRoute from "./components/ProtectedRoute";
 
-function Dashboard(){
+export default function App() {
 
-    return(
-
-        <div className="container mt-5">
-
-            <h1>
-
-                Dashboard
-
-            </h1>
-
-        </div>
-
-    );
-
-}
-
-export default function App(){
-
-    return(
+    return (
 
         <Routes>
 
-            <Route path="/" element={<Login/>}/>
-
-            <Route path="/login" element={<Login/>}/>
-
-            <Route path="/register" element={<Register/>}/>
-
-            <Route path="/waiting" element={<WaitingApproval/>}/>
+            <Route
+                path="/"
+                element={<Login />}
+            />
 
             <Route
+                path="/login"
+                element={<Login />}
+            />
 
+            <Route
+                path="/register"
+                element={<Register />}
+            />
+
+            <Route
+                path="/waiting"
+                element={<WaitingApproval />}
+            />
+
+            <Route
                 path="/dashboard"
-
                 element={
-
                     <ProtectedRoute>
 
-                        <Dashboard/>
+                        <Dashboard />
 
                     </ProtectedRoute>
-
                 }
-
             />
 
         </Routes>
