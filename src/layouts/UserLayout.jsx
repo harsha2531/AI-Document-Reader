bdaceaa8a084e060e261548db3db15a8f103f1c3
Harsha Nimeda Sirithunga <harshanimeda@gmail.com>
2026-07-22 00:29:49 +0530

    created admin and user layouts

---
+++ src/layouts/UserLayout.jsx
@@ -0,0 +1,33 @@
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
+
+export default function UserLayout({ children }) {
+
+    return (
+
+        <>
+
+            <Navbar />
+
+            <div className="d-flex">
+
+                <Sidebar />
+
+                <div
+                    className="p-4"
+                    style={{
+                        width: "100%"
+                    }}
+                >
+
+                    {children}
+
+                </div>
+
+            </div>
+
+        </>
+
+    );
+
+}
